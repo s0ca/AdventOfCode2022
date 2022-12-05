87e459292bbf672235e608db6a4e9b00b83a7553
s0ca <peyre.mathias@gmail.com>
2022-12-05 12:06:00 +0100

    Update day1_1.py

--- Day1/day1_1.py
+++ Day1/day1_1.py
@@ -1,4 +1,10 @@
-with open('1.txt') as f:
-    max_sum, total_sum = max(sum(int(x) for x in group.split('\n')) for group in f), sum(sorted([sum(int(x) for x in group.split('\n')) for group in f], reverse=True)[:3])
-print(max_sum)
-print(total_sum)
+X = [l.strip() for l in open ('1.txt')]
+FAT = []
+for elf in ('\n'.join(X)).split('\n\n'):
+    fat = 0
+    for x in elf.split('\n'):
+        fat += int(x)
+    FAT.append(fat)
+print(max(FAT))
+FAT=sorted(FAT)
+print(sum(FAT[-3:))
